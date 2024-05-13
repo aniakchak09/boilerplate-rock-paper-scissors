fb2b379c75ee688e2b460f8abfdfb32c2f28bfbd
Cretu Dan <dan.cretuas@yahoo.com>
2024-05-13 22:18:53 +0000

    idk

--- RPS.py
+++ RPS.py
@@ -1,4 +1,45 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+dftrain = pd.read_csv("RPS_train.csv")
+dfeval = pd.read_csv("RPS_eval.csv")
+y_train = dftrain.pop("outcome")
+y_eval = dfeval.pop("outcome")
+
+CATEGORICAL_COLUMNS = ["player1", "player2"]
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+    if shuffle:
+      ds = ds.shuffle(1000)
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    return ds
+  return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+linear_est.train(train_input_fn)
+result = linear_est.evaluate(eval_input_fn)
+
+clear_output()
+print(result["accuracy"])
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
